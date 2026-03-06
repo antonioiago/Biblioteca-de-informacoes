1d69c6038a1ef0e4b5e77a06e5101820d2e5cda3
antonioiago <antonio.portela@etec.sp.gov.br>
2026-03-06 16:09:13 -0300

    Atualização do arquivo

---
+++ Aula_0306/Aula 2/controller.php
@@ -0,0 +1,7 @@
+<?php
+ $nome = filter_input (type: INPUT_POST, var_name: "nome");
+
+ include 'data.php';
+
+ 
+?>
--- Aula_0306/Aula 2/index.php
+++ Aula_0306/Aula 2/index.php
@@ -1,4 +1,4 @@
-<form>
+<form action="controller.php" method="POST">
     <input type="text" name="nome" placeholder="MOME">
     <input type="submit" value="ENVIAR">
 </form>
